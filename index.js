25b100d84c9cf833136cd58b06368c79e643c84a
SPEED <js0976240@dt038567.otc.edu>
2023-04-26 13:07:11 -0500

    api stuff

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ import {configs} from './scripts.js';
 scripts();
 configs();
 
+import {config} from 'dotenv';
+import { Console } from 'console'
+config()
+console.log(process.env.API_KEY);
+
+let api_url = `
+//api url for moviedb with api key replaced by {process.env.API_KEY}
+`
 axios.get("https://dog.ceo/api/breeds/image/random")
 .then (res=>{
     console.log(res.data)
